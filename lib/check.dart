768448a1837900aef4c51a408eab983b9c0e340d
Saad Amjad <saadamjadofficial@gmail.com>
2024-03-05 20:48:05 +0500

    all set

--- lib/check.dart
+++ lib/check.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart';
 
 class chk extends StatefulWidget {
   const chk({super.key});
@@ -9,13 +11,25 @@ class chk extends StatefulWidget {
 }
 
 class _chkState extends State<chk> {
-  String name = "";
+  String name = "My Name";
   save() {
     FirebaseFirestore.instance
-        .collection("New Collection")
-        .doc("col")
-        .set({"name": "Maheen"}, SetOptions(merge: true)).then((value) {
+        .collection("Old Collection")
+        .doc("doc1")
+        .set({"name": "Amjad"}, SetOptions(merge: true)).then((value) {
       setState(() {});
+    });
+  }
+
+  getdata() {
+    FirebaseFirestore.instance
+        .collection("Old Collection")
+        .doc("doc1")
+        .get()
+        .then((value) {
+      setState(() {
+        name = value.get("name");
+      });
     });
   }
 
@@ -25,9 +39,25 @@ class _chkState extends State<chk> {
       backgroundColor: Colors.amberAccent,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: TextButton(onPressed: (){
-          save();
-        }, child: Text("name"),),
+        title: TextButton(
+          onPressed: () {
+            save();
+          },
+          child: Text("Click Me"),
+        ),
+      ),
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              getdata();
+            },
+            child: Text(
+              name,
+              style: TextStyle(color: Colors.white, fontSize: 48),
+            ),
+          )
+        ],
       ),
     );
   }
